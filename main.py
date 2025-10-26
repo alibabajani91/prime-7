7045e7c8aae3923d824131e2ded0f8c3e50738db
alibabajani <alibabajani310@gmail.com>
2025-10-26 17:41:37 +0330

    added a empty function and while loop

---
+++ main.py
@@ -0,0 +1,11 @@
+def number_check(number):
+    pass
+
+while True:
+    try:
+        number_entry = int(input("enter a number:"))
+    except:
+        print("enter a number please, not text")
+    if number_entry == any:
+        number_check(number_entry)
+
