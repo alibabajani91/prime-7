d414aa4daaf57f20b164e1fffeab0795cb174444
alibabajani <alibabajani310@gmail.com>
2025-10-26 18:27:15 +0330

    copmleted the code of function

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
-def number_check(number):
-    c_2 = number % 2
-    c_3 = number % 3
-    c_5 = number % 5
-    c_7 = number % 7
-    c_11 = number % 11
-    if c_2 == 0 or c_3 == 0 or c_5 == 0 or c_7 == 0 or c_11 == 0:
-        print(f"the {number} number is a complex number")
+def number_check(number_entry):
+    c_2 = number_entry % 2
+    c_3 = number_entry % 3
+    c_5 = number_entry % 5
+    c_7 = number_entry % 7
+    c_11 = number_entry % 11
+    if number_entry != 11:
+        if c_2 == 0 or c_3 == 0 or c_5 == 0 or c_7 == 0 or c_11 == 0:
+            print(f"{number_entry} is a complex number")
+        else:
+            print(f"{number_entry} is a prime number")
     else:
-        print(f"the {number} number is a prime number")
+        print("11 is a prime number")
 
 while True:
-
     number_entry = int(input("enter a number:"))
-    if number_entry == any:
-        number_check(number_entry)
+    number_check(number_entry)
 
