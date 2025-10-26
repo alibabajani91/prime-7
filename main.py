fb4afb395925bac747a6af2507b00d03191bc625
alibabajani <alibabajani310@gmail.com>
2025-10-26 17:48:13 +0330

    added some variable to check number

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 def number_check(number):
-    pass
+    c_2 = number % 2
+    c_3 = number % 3
+    c_5 = number % 5
+    c_7 = number % 7
+    c_11 = number % 11
+    if c_2 == 0 or c_3 == 0 or c_5 == 0 or c_7 == 0 or c_11 == 0:
+        print(f"the {number} number is a complex number")
+    else:
+        print(f"the {number} number is a prime number")
 
 while True:
     try:
