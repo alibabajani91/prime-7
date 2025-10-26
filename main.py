ef6629ba15569bfbff01d1ec4da9e74d24311371
alibabajani <alibabajani310@gmail.com>
2025-10-26 19:21:07 +0330

    debuged some bugs

--- main.py
+++ main.py
@@ -4,13 +4,15 @@ def number_check(number_entry):
     c_5 = number_entry % 5
     c_7 = number_entry % 7
     c_11 = number_entry % 11
-    if number_entry != 11:
-        if c_2 == 0 or c_3 == 0 or c_5 == 0 or c_7 == 0 or c_11 == 0:
-            print(f"{number_entry} is a complex number")
-        else:
-            print(f"{number_entry} is a prime number")
+    if number_entry == 3 or number_entry == 2 or number_entry == 5 or number_entry == 7 or number_entry == 11:
+        print(f"{number_entry} is a prime number")
+    if number_entry == 1:
+        print("not defined")
+    if c_2 == 0 or c_3 == 0 or c_5 == 0 or c_7 == 0 or c_11 == 0:
+        print(f"{number_entry} is a complex number")
     else:
-        print("11 is a prime number")
+        print(f"{number_entry} is a prime number")
+
 
 while True:
     try:
