8eb0c0a56af9a461fb3f635bd1e0189b94fbc99a
alibabajani <alibabajani310@gmail.com>
2025-10-29 11:34:41 +0330

    added exit command and return based the function

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ def number_check(number):
     c_7 = number % 7
     c_11 = number % 11
     if number == 3 or number == 2 or number == 5 or number == 7 or number == 11:
-        print(f"{number} is a prime number")
+        return 1
     elif number == 1:
-        print("not defined")
+        return 2
     elif c_2 == 0 or c_3 == 0 or c_5 == 0 or c_7 == 0 or c_11 == 0:
-        print(f"{number} is a complex number")
+        return 0
     else:
-        print(f"{number} is a prime number")
+        return 1
 
 
 
@@ -23,18 +23,30 @@ def some_number():
             print("number 1 must be smaller than number 2")
         else:
             i = number_1
-            number_check(i)
             while i < number_2:
                 i += 1
-                number_check(i)
+                result = number_check(i)
+                if result == 1 :
+                    print(f"{i} = prime")
+                elif result == 2:
+                    print("not define")
+                else:
+                    print(f"{i} = complex")
     except ValueError:
         print("enter a number,not text")
 
 
 def one_number():
     try:
+        
         number_entry = int(input("enter a number:"))
-        number_check(number_entry)
+        result = number_check(number_entry)
+        if result == 1 :
+            print(f"{number_entry} = prime")
+        elif result == 2:
+            print("not define")
+        else:
+            print(f"{number_entry} = complex")
     except ValueError:
         print("enter a number,not text")
 
@@ -45,9 +57,10 @@ while True:
         some_number()
     elif cmd == "one number":
         one_number()
+    elif cmd == exit:
+        exit()
     elif cmd == "help":
         with open("readme.txt", "r") as f:
             print(f.read())
-        
     else:
         print("this command not defined, or maybe you write it wrong!")
